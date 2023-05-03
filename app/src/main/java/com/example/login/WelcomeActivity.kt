72b875ee62729785d9c9a36db801ebdb5572f93d
Maleesha Dilshan <maleeshahirug@gmail.com>
2023-05-03 13:20:49 +0530

    binding enabled

--- app/src/main/java/com/example/login/WelcomeActivity.kt
+++ app/src/main/java/com/example/login/WelcomeActivity.kt
@@ -1,11 +1,23 @@
 package com.example.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.login.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnWelLogin.setOnClickListener {
+            intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
